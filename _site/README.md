f9a26c2d35de7550f85f7b8f5f7f25f53e8c870a
Mr.lu <weixiaobao5603169@gmail.com>
2014-09-20 12:04:04 +0800

    ont time

---
+++ _site/README.md
@@ -0,0 +1,9 @@
+Blog
+
+Lu Dongwei's Personal Website
+
+这是我的第一个博客使用github+jekyll 
+
+
+
+
---
+++ _site/assert/js/seajs/config.js
@@ -0,0 +1,8 @@
+seajs.config({
+    base:"http://ldw-blog.qiniudn.com/js/",	
+	alias:{
+       "jquery":"jquery-1.9.1-seajs",
+       "bxslider":"bxslider-4.1.2-seajs",
+       "handlebars":"handlebars-v1.3.0-seajs"
+	}
+})
